19c1075f2c9c2cee9f51fead7f8139eab6737ee8
joydeep <defyjoy@users.noreply.github.com>
2026-02-17 21:15:18 +0530

    Initial nixos config

---
+++ etc/nixos/home.nix
@@ -0,0 +1,41 @@
+{ config, pkgs , ...}:
+
+{
+   home.username = "jroychowdhury";
+   home.homeDirectory = "/home/jroychowdhury";
+   home.stateVersion = "25.11";
+
+   
+  programs.zsh = {
+    enable = true;
+
+     # Nice defaults (optional but recommended)
+    enableCompletion = true;
+    autosuggestion.enable = true;  # zsh-autosuggestions
+    syntaxHighlighting.enable = true; # zsh-syntax-highlighting (optional but great):
+    # autosuggestions.enable = true;
+    # syntaxHighlighting.enable = true;
+
+    initContent = ''
+      alias nrs="sudo nixos-rebuild switch"
+      alias hi="Hello Joy! Nice to meet you!"
+      alias nixls="sudo nix-env -p /nix/var/nix/profiles/system --list-generations"
+      alias nixdel="sudo nix-env -p /nix/var/nix/profiles/system --delete-generations"
+    '';
+
+    oh-my-zsh = {
+	  	enable = true;
+		
+    	plugins = [
+      	  "git"
+      	  "globalias"
+    	];
+		theme = "fino-time";
+    };
+  };
+
+  home.packages = with pkgs; [
+    zsh
+    oh-my-zsh
+  ];
+}
