259fcedb67eec06415cc3df0593a0ee95c755bfb
joydeep roychowdhury <defyjoy@users.noreply.github.com>
2026-02-24 07:13:07 +0530

    (chore): update chezmoi configuration destination in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,7 +13,7 @@ sudo mount --mkdir /dev/sda1 /mnt/boot
 sudo mount -t efivarfs efivarfs /sys/firmware/efi/efivars
 
 echo "applying chezmoi configuration"
-sudo chezmoi apply --source ~/.local/share/chezmoi --destination /
+sudo chezmoi apply --source ~/.local/share/chezmoi --destination /mnt
 
 echo "generating hardware configuration"
 sudo nixos-generate-config --root /mnt --show-hardware-config > /mnt/etc/nixos/hardware-configuration.nix
